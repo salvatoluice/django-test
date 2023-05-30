3bb8c64b4dc9b88a7d55e8e6963ddfb6a83845b4
salvatoluice <salvatoluice5@gmail.com>
2023-05-30 11:09:33 +0300

    URL mapper definition

--- mytestsite/urls.py
+++ mytestsite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns += [
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
