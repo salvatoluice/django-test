f9056038361e11e2e7959768750d90cfee678d2f
salvatoluice <salvatoluice5@gmail.com>
2023-05-30 11:00:10 +0300

    Add redirect url

--- mytestsite/urls.py
+++ mytestsite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,8 @@ urlpatterns = [
 
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
