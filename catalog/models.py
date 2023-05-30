35e9f4fc9266cc3371c9c19b5369f6816a1bb92e
salvatoluice <salvatoluice5@gmail.com>
2023-05-30 16:25:35 +0300

    comment

--- catalog/models.py
+++ catalog/models.py
@@ -22,3 +22,6 @@ class MyModelName(models.Model):
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
         return self.my_field_name
+# A model can have arbitrary number of fields, of any type - each one represents a column of data that we want to store in 
+#  in one of our database tables. Each database record(row) will consist one of each value. i.e.
+# my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
